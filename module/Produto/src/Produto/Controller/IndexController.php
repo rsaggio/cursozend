a75a845a36749416c760fe6c11aff252b8e8c4bc
Renan Saggio <renan.saggio@gmail.com>
2015-09-01 20:33:47 -0300

    Capitulo 1 aula 2

---
+++ module/Produto/src/Produto/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Produto\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class IndexController extends AbstractActionController {
+
+
+	public function indexAction() {
+		echo "Bem vindos ao curso de zend framework";
+	}
+
+ }
+
+?>
