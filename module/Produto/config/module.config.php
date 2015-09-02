f87188ff7dc85c46722b92ce2c4564b06c094e30
Renan Saggio <renan.saggio@gmail.com>
2015-09-01 22:43:07 -0300

    Capitulo 1 aula 3

--- module/Produto/config/module.config.php
+++ module/Produto/config/module.config.php
@@ -1,3 +1,23 @@
 <?php
 return array(
+	'router' => array (
+		'routes' => array (
+			'application' => array (
+				'type' => 'Literal',
+				'options' => array (
+					'route' => '/app',
+					'defaults' => array (
+						'__NAMESPACE__' => 'Produto\Controller',
+						'controller' => 'Index',
+						'action' => 'Index'
+					),
+				),
+			),
+		),
+	),
+	'controllers' => array(
+        'invokables' => array(
+            'Produto\Controller\Index' => 'Produto\Controller\IndexController'
+        ),
+    ),
 );
--- module/Produto/src/Produto/Controller/IndexController.php
+++ module/Produto/src/Produto/Controller/IndexController.php
@@ -6,7 +6,6 @@ use Zend\Mvc\Controller\AbstractActionController;
 
 class IndexController extends AbstractActionController {
 
-
 	public function indexAction() {
 		echo "Bem vindos ao curso de zend framework";
 	}
